41f3aec1aff8221a65fc656ef4590d67efe7fb2b
nathanjaekim25-commits <nathanjaekim.25@gmail.com>
2025-09-25 23:03:48 -0400

    Add files via upload

---
+++ headfunc.py
@@ -0,0 +1,5 @@
+# This file contains all the data for the headphone quiz answers (aside from the images and links)
+
+def headphoneFunction:
+    
+
---
+++ info.py
@@ -0,0 +1,84 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpg"
+about_me = "I'm Nathan Kim. I am a first year at Georgia Tech who is interested in product design, systems optimization, and loves making headphones."
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+instagram_image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1200px-Instagram_icon.png?20200512141346"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/nathan-kim-238180323"
+my_instagram_url = "https://www.instagram.com/nathankim.art/"
+my_email_address = "nkim416@gatech.edu"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Industrial Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': '2029',
+    'GPA': 'N/A'
+}
+course_data = {
+    "code":["CS 1301", "ENGL 1102", "MATH 1554", "PSYCH 1101", "ECON 2106"], 
+    "names":["Intro to CS", "English Composition II", "Linear Algebra", "Intro to Gen Psych", "Principles of Microeconomics"], 
+    "semester_taken":["1st", "1st", "1st", "1st", "1st"],
+    "skills":["Learning web development!", "Monsters and Others", "Matrices and vectors...", "How people think", "Does money make the world go around?"],
+    }
+experience_data = {
+    "Shadow at Match.com": (["- Shadowed and performed analysis in strategic leadership and optimization",
+                                                                          "- Analysis of recommendation algorithm systems", "- Attended high level meetings"],"Images/match.jpg"),
+    "Research intern at The Children's Institute":(["- Researched classroom technology solutions for aurally sensititve neurodivergent students",
+                                                           "- Interviewed students to collect data and designed possible tools for increasing productivity"],"Images/tci.jpg"),
+    "Video Game Analysis":(["- Assessed commercial potential of influencer driven data to forecast game sales"],"Images/games.jpg")
+
+}
+
+projects_data = {
+    "Resonance": "Created a headphone prototype for a headphone that will help neurodivergent students connect with teachers and peers in a noisy classroom.",
+}
+
+software_data = {
+    "Python": 10,
+    "Procreate": 90,
+    "Blender": 80,
+}
+
+#CHANGE BELOW (OPTIONAL)
+software_icons = {
+    "Python": "🐍",
+    "Procreate": "🖌",
+    "Blender": "🧊",
+}
+spoken_icons = {"English": "🇺🇸",
+    "Korean": "🇰🇷",
+    "Mandarin":"🇨🇳"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Korean": "Fluent",
+    "Mandarin": "Intermediate",
+}
+leadership_data = {
+    "Leader of AAT": (["- Nonprofit with goals of providing STEM and arts education to underprivilaged students in NJ"],"Images/aat.jpg"),
+
+}
+activity_data={
+    "Visual and Performing Arts": ["- Skilled at Procreate, Illustrator, Painting (oil, acrylic, poster, water), and Canva", 
+            "- Avid singer and actor"]
+}
+
+
+
+# Quiz images
+money = "Images/money.jpg"
+openbackImage = "Images/obImage.jpg"
+btImage = "Images/btImage.jpg"
+
